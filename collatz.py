0220ef374f4a8703e9d7b3227792d96dc4c8ed72
JudgeCastle <56881291+JudgeCastle@users.noreply.github.com>
2023-04-07 18:52:28 -0400

    Add files via upload

---
+++ collatz.py
@@ -0,0 +1,21 @@
+import sys
+def collatz(number):
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+    elif number % 2 == 1:
+        print(3 * number + 1)
+        return 3 * number + 1
+
+
+def uservalue():
+    try:
+        userinput = int(input('Input a number:'))
+    except ValueError:
+        print('You did not input a number.')
+        sys.exit()
+    while userinput != 1:
+        userinput = collatz(userinput)
+        
+
+uservalue()
